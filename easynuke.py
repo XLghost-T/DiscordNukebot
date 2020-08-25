3bc6bc683617280bfe1c119f6f63fea652e8b542
XLghost-T <70231806+XLghost-T@users.noreply.github.com>
2020-08-25 19:59:29 +0100

    Update easynuke.py

--- easynuke.py
+++ easynuke.py
@@ -1,6 +1,6 @@
 #BOT CONFIG
-token = ""
-prefix = ""
+token = "NzQ3MTUwMTcyMDkzNDE1NTk3.X0Kr0g.63xa6_2rNVGpI5qSijw3yjcnvzw"
+prefix = "$"
 
 #NUKE CONFIG
 spam_messages = ["@everyone nuked", "@everyone get nuked"]
